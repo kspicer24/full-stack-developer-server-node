40f3da0d6bbdfcce7459644a6628ab24741bb306
kspicer24 <kristispicer124@gmail.com>
2022-03-30 22:52:59 -0400

    a8 up through e3.4

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import helloController
+    from "./controllers/hello-controller.js";
+import userController   from "./controllers/user-controller.js";
+
+const app = express();
+app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
+helloController(app);
+userController(app);
+
+app.listen(4000);
